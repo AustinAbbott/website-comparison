3ffda9a98bf9bfa42f842b0f7708f93586ca9287
Austin Abbott <magicaustin51547@gmail.com>
2020-10-12 21:14:28 -0600

    Remove create react app extras, add basic inputs to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello</h1>
+        <div>
+          <div>
+            <label>URL1:</label>
+            <input type="text" id="URL1" oninput="fetchDom()" />
+          </div>
+          <div>
+            <label>URL2:</label>
+            <input type="text" id="URL2" oninput="fetchDom()" />
+            <br></br>
+            <br></br>
+            <buttton>Submit</buttton>
+          </div>
+        </div>
       </header>
     </div>
   );
