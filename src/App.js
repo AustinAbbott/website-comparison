4810bff268bc624a506f0814cae2c8d3602442f4
Austin Abbott <magicaustin51547@gmail.com>
2020-10-21 20:28:12 -0600

    Create function to store/update URLs, function for form submission

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import React from "react";
-// import logo from './logo.svg';
-import consoleLog from "./functions.js";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { inputsFull: false };
+    this.state = { inputsFull: false, URL1: "", URL2: "" };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  testFunc = (event) => {
-    event.preventDefault();
-    console.log("yes yes yes");
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
   };
 
+  sendURLs = (e) => {
+    e.preventDefault();
+    console.log(this.state.URL1, this.state.URL2);
+  };
 
   render() {
-    
     return (
       <div className="App">
         <header className="App-header">
           <h2>Compare Two Websites</h2>
-          <form onSubmit={this.testFunc}>
+          <form onSubmit={this.sendURLs}>
             <div>
               <div>
                 <label>URL1:</label>
-                <input type="text" id="URL1" />
+                <input
+                  type="text"
+                  id="URL1"
+                  name="URL1"
+                  value={this.state.URL1}
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
                 <label>URL2:</label>
-                <input type="text" id="URL2" />
+                <input
+                  type="text"
+                  id="URL2"
+                  name="URL2"
+                  value={this.state.URL2}
+                  onChange={this.handleChange}
+                />
                 <br></br>
                 <br></br>
                 <button>Send It!</button>
